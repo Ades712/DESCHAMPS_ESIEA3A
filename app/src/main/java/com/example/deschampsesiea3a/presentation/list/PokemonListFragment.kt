6ebeb1b47f6d9608c6845a00926b89975e1e982e
Ades <adeschamps@et.esiea.fr>
2021-05-23 21:40:39 +0200

    Add image for Pokemon

--- app/src/main/java/com/example/deschampsesiea3a/presentation/list/PokemonListFragment.kt
+++ app/src/main/java/com/example/deschampsesiea3a/presentation/list/PokemonListFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -27,6 +29,7 @@ class PokemonListFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private val adapter = PokemonAdapter(listOf(), ::onClickedPokemon)
+    private val viewModel: PokemonListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,22 +48,10 @@ class PokemonListFragment : Fragment() {
             adapter = this@PokemonListFragment.adapter
         }
 
-
-
-        Singletons.pokeApi.getPokemonList("151").enqueue(object: Callback<PokemonListResponse>{
-            override fun onFailure(call: Call<PokemonListResponse>, t: Throwable) {
-            }
-
-            override fun onResponse(call: Call<PokemonListResponse>, response: Response<PokemonListResponse>) {
-                if(response.isSuccessful && response.body() != null){
-                    val pokemonResponse: PokemonListResponse? = response.body()!!
-                    adapter.updateList(pokemonResponse!!.results)
-                }
-            }
-
-
+        viewModel.pokeList.observe(viewLifecycleOwner, Observer {
+            list ->
+            adapter.updateList(list)
         })
-
 
     }
     private fun onClickedPokemon(id: Int) {
